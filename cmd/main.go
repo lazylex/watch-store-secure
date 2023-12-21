9b7b7f1c9deb57519c2a939bfc84e20c29e352e4
lazylex <lazylex@mail.ru>
2023-12-21 22:54:10 +0300

    В сервис внедрены метрики

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/lazylex/watch-store/secure/internal/config"
+	"github.com/lazylex/watch-store/secure/internal/logger"
+	prometheusMetrics "github.com/lazylex/watch-store/secure/internal/metrics"
+	"github.com/lazylex/watch-store/secure/internal/service"
+)
 
+func main() {
+	cfg := config.MustLoad()
+	log := logger.MustCreate(cfg.Env, cfg.Instance)
+	metrics := prometheusMetrics.MustCreate(&cfg.Prometheus, log)
+	_ = service.New(metrics.Service)
+
+	// TODO удалить, когда будет запущен http-сервер для обработки внешних запросов
+	c := make(chan struct{})
+	_ = <-c
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -4,9 +4,12 @@ import (
 	"errors"
 	"github.com/lazylex/watch-store/secure/internal/domain/value_objects/login"
 	"github.com/lazylex/watch-store/secure/internal/dto"
+	"github.com/lazylex/watch-store/secure/internal/ports/metrics/service"
 )
 
-type Service struct{}
+type Service struct {
+	metrics service.MetricsInterface
+}
 
 var (
 	ErrAuthenticationData = serviceError("Неправильный логин или пароль")
@@ -16,9 +19,17 @@ func serviceError(text string) error {
 	return errors.New("service: " + text)
 }
 
+// TODO заменить параметры на Option (при необходимости)
+
+// New конструктор для сервиса
+func New(metrics service.MetricsInterface) *Service {
+	return &Service{metrics: metrics}
+}
+
 // Login совершает логин пользователя (сервиса) по переданным в dto логину и паролю. Воввращает токен сессии и ошибку
 func (s *Service) Login(dto *dto.LoginPasswordDTO) (string, error) {
 	if !s.isAuthenticated(dto) {
+		s.metrics.AuthenticationErrorInc()
 		return "", ErrAuthenticationData
 	}
 	token := s.createToken()
@@ -31,6 +42,7 @@ func (s *Service) Login(dto *dto.LoginPasswordDTO) (string, error) {
 // действителен на время действия сессии пользователя с логином login
 func (s *Service) login(token string, login login.Login) {
 	// TODO implement
+	s.metrics.LoginInc()
 }
 
 // isAuthenticated проверяет, является ли пользователь (сервис) аутентифицированным
