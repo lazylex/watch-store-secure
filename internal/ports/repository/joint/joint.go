62e78f5a77aa95bca982878a47fa294b49a09790
lazylex <lazylex@mail.ru>
2024-04-23 15:08:08 +0300

    Частично упорядочен интерфейс

--- internal/ports/repository/joint/joint.go
+++ internal/ports/repository/joint/joint.go
@@ -6,6 +6,7 @@ import (
 	"github.com/lazylex/watch-store/secure/internal/domain/value_objects/account_state"
 	"github.com/lazylex/watch-store/secure/internal/domain/value_objects/login"
 	"github.com/lazylex/watch-store/secure/internal/dto"
+	"github.com/lazylex/watch-store/secure/internal/ports/service"
 )
 
 type ServiceInterface interface {
@@ -25,17 +26,9 @@ type LoginInterface interface {
 // TODO добавить удаление разрешений, ролей и групп
 
 type RBACInterface interface {
-	CreatePermission(context.Context, dto.PermissionWithoutNumberDTO) error
-	CreateRole(context.Context, dto.NameAndServiceWithDescriptionDTO) error
-	CreateGroup(context.Context, dto.NameAndServiceWithDescriptionDTO) error
-
-	AssignRoleToAccount(context.Context, dto.RoleServiceNamesWithUserIdDTO) error
-	AssignGroupToAccount(context.Context, dto.GroupServiceNamesWithUserIdDTO) error
-	AssignInstancePermissionToAccount(context.Context, dto.InstanceAndPermissionNamesWithUserIdDTO) error
-
-	AssignRoleToGroup(context.Context, dto.GroupRoleServiceNamesDTO) error
-	AssignPermissionToRole(context.Context, dto.PermissionRoleServiceNamesDTO) error
-	AssignPermissionToGroup(context.Context, dto.GroupPermissionServiceNamesDTO) error
+	service.RBACCreateInterface
+	service.RBACAssignToAccountInterface
+	service.RBACAssignInterface
 
 	GetServicePermissionsForAccount(context.Context, dto.ServiceNameWithUserIdDTO) ([]dto.PermissionWithoutServiceDTO, error)
 	GetServicePermissionsNumbersForAccount(context.Context, dto.ServiceNameWithUserIdDTO) ([]int, error)
