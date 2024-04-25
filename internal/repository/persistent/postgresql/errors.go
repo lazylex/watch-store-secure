e592dba48553a1896bc0e9f3069315f66bea5899
lazylex <lazylex@mail.ru>
2024-04-25 13:15:45 +0300

    Обработка ошибок перенесена в отдельный файл

---
+++ internal/repository/persistent/postgresql/errors.go
@@ -0,0 +1,43 @@
+package postgresql
+
+import (
+	"github.com/jackc/pgx"
+	"github.com/lazylex/watch-store/secure/internal/lexerr"
+	"strings"
+)
+
+// processExecResult возвращает ошибку ErrZeroRowsAffected, если при выполнении запроса не было затронуто ни одной
+// строки. В противном случае возвращает ошибку, адаптированную под структуру ошибки lexerr.Persistent
+func (p *PostgreSQL) processExecResult(commandTag pgx.CommandTag, err error) error {
+	origin := lexerr.GetFrame(2).Function
+	origin = origin[strings.LastIndex(origin, ".")+1:]
+	if err != nil && strings.HasPrefix(err.Error(), "ERROR: duplicate key value violates unique constraint") {
+		return lexerr.ErrDuplicateKeyValue.WithOrigin(origin)
+	}
+
+	if commandTag.RowsAffected() == 0 {
+		return lexerr.ErrZeroRowsAffected
+	}
+
+	return adaptErrSkipFrames(err, 3)
+}
+
+// adaptErr переводит пришедшую ошибку к структурированной ошибке lexerr.Persistent
+func adaptErr(err error) error {
+	return adaptErrSkipFrames(err, 2)
+}
+
+// adaptErrSkipFrames переводит пришедшую ошибку к структурированной ошибке lexerr.Persistent с учетом последовательности
+// вызова функций
+func adaptErrSkipFrames(err error, skip int) error {
+	if err == nil {
+		return nil
+	}
+	origin := lexerr.GetFrame(skip).Function
+	origin = origin[strings.LastIndex(origin, ".")+1:]
+	if strings.HasPrefix(err.Error(), "ERROR: duplicate key value violates unique constraint") {
+		return lexerr.ErrDuplicateKeyValue.WithOrigin(origin)
+	}
+
+	return lexerr.FullPersistentError("", origin, err)
+}
--- internal/repository/persistent/postgresql/postgre.go
+++ internal/repository/persistent/postgresql/postgre.go
@@ -7,10 +7,8 @@ import (
 	"github.com/lazylex/watch-store/secure/internal/domain/value_objects/account_state"
 	loginVO "github.com/lazylex/watch-store/secure/internal/domain/value_objects/login"
 	"github.com/lazylex/watch-store/secure/internal/dto"
-	"github.com/lazylex/watch-store/secure/internal/lexerr"
 	"log/slog"
 	"os"
-	"strings"
 )
 
 type PostgreSQL struct {
@@ -464,40 +462,4 @@ func (p *PostgreSQL) GetAccountsLoginsByState(ctx context.Context, state account
 	}
 
 	return result, nil
-}
-
-// processExecResult возвращает ошибку ErrZeroRowsAffected, если при выполнении запроса не было затронуто ни одной
-// строки. В противном случае возвращает ошибку, адаптированную под структуру ошибки lexerr.Persistent
-func (p *PostgreSQL) processExecResult(commandTag pgx.CommandTag, err error) error {
-	origin := lexerr.GetFrame(2).Function
-	origin = origin[strings.LastIndex(origin, ".")+1:]
-	if err != nil && strings.HasPrefix(err.Error(), "ERROR: duplicate key value violates unique constraint") {
-		return lexerr.ErrDuplicateKeyValue.WithOrigin(origin)
-	}
-
-	if commandTag.RowsAffected() == 0 {
-		return lexerr.ErrZeroRowsAffected
-	}
-
-	return adaptErrSkipFrames(err, 3)
-}
-
-// adaptErr переводит пришедшую ошибку к структурированной ошибке lexerr.Persistent
-func adaptErr(err error) error {
-	return adaptErrSkipFrames(err, 2)
-}
-
-// adaptErrSkipFrames переводит пришедшую ошибку к структурированной ошибке lexerr.Persistent с учетом последовательности
-// вызова функций
-func adaptErrSkipFrames(err error, skip int) error {
-	if err == nil {
-		return nil
-	}
-	origin := lexerr.GetFrame(skip).Function
-	origin = origin[strings.LastIndex(origin, ".")+1:]
-	if strings.HasPrefix(err.Error(), "ERROR: duplicate key value violates unique constraint") {
-		return lexerr.ErrDuplicateKeyValue.WithOrigin(origin)
-	}
-
-	return lexerr.FullPersistentError("", origin, err)
 }
